ecb3e4aa99e33386accc12ebf46755df070d96c1
Ilija Studen <ilija.studen@activecollab.com>
2015-10-07 22:25:02 +0200

    Added URL field

---
+++ src/Field/Composite/UrlField.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace ActiveCollab\DatabaseStructure\Field\Composite;
+
+use ActiveCollab\DatabaseStructure\TypeInterface;
+use ActiveCollab\DatabaseStructure\FieldInterface;
+use ActiveCollab\DatabaseStructure\Index;
+use ActiveCollab\DatabaseStructure\Field\Scalar\StringField;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\RequiredInterface;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\RequiredInterface\Implementation as RequiredInterfaceImplementation;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\UniqueInterface;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\UniqueInterface\Implementation as UniqueInterfaceImplementation;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\ModifierInterface;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\ModifierInterface\Implementation as ModifierInterfaceImplementation;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\AddIndexInterface;
+use ActiveCollab\DatabaseStructure\Field\Scalar\Traits\AddIndexInterface\Implementation as AddIndexInterfaceImplementation;
+use InvalidArgumentException;
+
+/**
+ * @package ActiveCollab\DatabaseStructure\Field\Composite
+ */
+class UrlField extends Field implements RequiredInterface, UniqueInterface, ModifierInterface, AddIndexInterface
+{
+    use RequiredInterfaceImplementation, UniqueInterfaceImplementation, ModifierInterfaceImplementation, AddIndexInterfaceImplementation;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string|null
+     */
+    private $default_value;
+
+    /**
+     * @param string      $name
+     * @param string|null $default_value
+     * @param boolean     $add_index = false
+     */
+    public function __construct($name, $default_value = null, $add_index = false)
+    {
+        if (empty($name)) {
+            throw new InvalidArgumentException("Value '$name' is not a valid foreign key name");
+        }
+
+        $this->name = $name;
+        $this->default_value = $default_value;
+        $this->addIndex($add_index);
+
+        if ($this->default_value !== null) {
+            $this->modifier('trim');
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Return default field value
+     *
+     * @return string|null
+     */
+    public function getDefaultValue()
+    {
+        return $this->default_value;
+    }
+
+    /**
+     * @return FieldInterface[]
+     */
+    public function getFields()
+    {
+        $url_field = new StringField($this->getName(), $this->getDefaultValue());
+
+        if ($this->getDefaultValue() !== null) {
+            $url_field->modifier('trim');
+        }
+
+        if ($this->isRequired()) {
+            $url_field->required();
+        }
+
+        if ($this->isUnique()) {
+            $url_field->unique(...$this->getUniquenessContext());
+        }
+
+        return [$url_field];
+    }
+
+    /**
+     * Prepare validator lines
+     *
+     * @param string $indent
+     * @param array  $result
+     */
+    public function getValidatorLines($indent, array &$result)
+    {
+        $result[] = $indent . '$validator->url(' . var_export($this->getName(), true) . ', ' . ($this->getDefaultValue() === null ? 'true' : 'false') . ');';
+    }
+
+    /**
+     * Method that is called when field is added to a type
+     *
+     * @param TypeInterface $type
+     */
+    public function onAddedToType(TypeInterface &$type)
+    {
+        if ($this->getAddIndex()) {
+            $index_fields = [$this->name];
+
+            if (count($this->getAddIndexContext())) {
+                $index_fields = array_merge($index_fields, $this->getAddIndexContext());
+            }
+
+            $type->addIndex(new Index($this->name, $index_fields, $this->getAddIndexType()));
+        }
+    }
+}
---
+++ test/src/UrlFieldTest.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace ActiveCollab\DatabaseStructure\Test;
+
+use ActiveCollab\DatabaseStructure\Type;
+use ActiveCollab\DatabaseStructure\IndexInterface;
+use ActiveCollab\DatabaseStructure\Field\Composite\UrlField;
+use ActiveCollab\DatabaseStructure\Field\Scalar\StringField;
+
+/**
+ * @package ActiveCollab\DatabaseStructure\Test
+ */
+class UrlFieldTest extends TestCase
+{
+    /**
+     * Test if name can be set
+     */
+    public function testName()
+    {
+        $this->assertEquals('homepage_url', (new UrlField('homepage_url'))->getName());
+    }
+
+    /**
+     * Test if NULL is the default value
+     */
+    public function testNullIsDefaultValue()
+    {
+        $this->assertNull((new UrlField('homepage_url'))->getDefaultValue());
+    }
+
+    /**
+     * Test no modifier when default URL is NULL
+     */
+    public function testNoModifierWhenNullIsDefaultValue()
+    {
+        $this->assertNull((new UrlField('homepage_url'))->getModifier());
+    }
+
+    /**
+     * Test if URL is trimmed when not null by default
+     */
+    public function testValueIsTrimmedByDefault()
+    {
+        $this->assertEquals('trim', (new UrlField('homepage_url', 'not null'))->getModifier());
+    }
+
+    /**
+     * Test if URL can be added to a type
+     */
+    public function testUrlCanBeAddedToType()
+    {
+        $type = (new Type('writers'))->addField(new UrlField('homepage_url'));
+
+        $this->assertArrayHasKey('homepage_url', $type->getFields());
+        $this->assertInstanceOf(UrlField::class, $type->getFields()['homepage_url']);
+    }
+
+    /**
+     * Test if URL field does not add an index by default
+     */
+    public function testUrlFieldDoesNotAddIndexByDefault()
+    {
+        $type = (new Type('writers'))->addField(new UrlField('homepage_url'));
+
+        $this->assertCount(0, $type->getIndexes());
+    }
+
+    /**
+     * Test if URL index is added to the type when requested
+     */
+    public function testUrlFieldAddsIndexWhenRequested()
+    {
+        $type = (new Type('writers'))->addField(new UrlField('homepage_url', null, true));
+
+        $this->assertArrayHasKey('homepage_url', $type->getIndexes());
+
+        $url_index = $type->getIndexes()['homepage_url'];
+
+        $this->assertInstanceOf(IndexInterface::class, $url_index);
+        $this->assertEquals(IndexInterface::INDEX, $url_index->getIndexType());
+    }
+
+    /**
+     * Test if unique URL index is added to the type when requested
+     */
+    public function testUrlFieldAddsUniqueIndexWhenRequested()
+    {
+        $type = (new Type('writers'))->addField((new UrlField('homepage_url', null, true))->unique());
+
+        $this->assertArrayHasKey('homepage_url', $type->getIndexes());
+
+        $url_index = $type->getIndexes()['homepage_url'];
+
+        $this->assertInstanceOf(IndexInterface::class, $url_index);
+        $this->assertEquals(IndexInterface::UNIQUE, $url_index->getIndexType());
+    }
+
+    /**
+     * Test if unique index properly alters index added by the URL field
+     */
+    public function testUniqueContextAddsFieldsToTheIndex()
+    {
+        $type = (new Type('applications'))->addField((new UrlField('homepage_url', null, true))->unique('application_id', 'shard_id'));
+
+        $this->assertArrayHasKey('homepage_url', $type->getIndexes());
+
+        $name_index = $type->getIndexes()['homepage_url'];
+
+        $this->assertInstanceOf(IndexInterface::class, $name_index);
+
+        $this->assertEquals('homepage_url', $name_index->getName());
+        $this->assertEquals(['homepage_url', 'application_id', 'shard_id'], $name_index->getFields());
+    }
+
+    /**
+     * Make sure that resulting string field is required and / or unique when name is required and / or unique
+     */
+    public function testUrlProducesRequiredAndUniqueStringWhenRequiredAndUnique()
+    {
+        $name_field = (new UrlField('homepage_url'))->required()->unique('context_filed_1');
+
+        /** @var StringField $string_field */
+        $string_field = $name_field->getFields()[0];
+
+        $this->assertInstanceOf(StringField::class, $string_field);
+
+        $this->assertTrue($string_field->isRequired());
+        $this->assertTrue($string_field->isUnique());
+        $this->assertEquals(['context_filed_1'], $string_field->getUniquenessContext());
+    }
+}
