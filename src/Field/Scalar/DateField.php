dce5a348bdc82070135e7418abc76a6304f22508
Ilija Studen <ilija.studen@activecollab.com>
2015-10-10 15:28:34 +0200

    Cast date and date time values to DateValue and DateTimeValue insatnces when hydrating objects

--- src/Field/Scalar/DateField.php
+++ src/Field/Scalar/DateField.php
@@ -7,4 +7,14 @@ namespace ActiveCollab\DatabaseStructure\Field\Scalar;
  */
 class DateField extends Field
 {
+    /**
+     * Return value casting code
+     *
+     * @param  string $variable_name
+     * @return string
+     */
+    public function getCastingCode($variable_name)
+    {
+        return 'new \\ActiveCollab\\DateValue\\DateValue($' . $variable_name . ')';
+    }
 }
--- src/Field/Scalar/DateTimeField.php
+++ src/Field/Scalar/DateTimeField.php
@@ -7,4 +7,14 @@ namespace ActiveCollab\DatabaseStructure\Field\Scalar;
  */
 class DateTimeField extends Field
 {
+    /**
+     * Return value casting code
+     *
+     * @param  string $variable_name
+     * @return string
+     */
+    public function getCastingCode($variable_name)
+    {
+        return 'new \\ActiveCollab\\DateValue\\DateTimeValue($' . $variable_name . ', \'UTC\')';
+    }
 }
