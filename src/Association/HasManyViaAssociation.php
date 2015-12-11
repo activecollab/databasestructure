1fd64ab57ba5bae44d06a6d2199e4d7aeb4bc59b
Ilija Studen <ilija.studen@activecollab.com>
2015-12-11 17:00:12 +0100

    Override get finder and count method builders

--- src/Association/HasManyViaAssociation.php
+++ src/Association/HasManyViaAssociation.php
@@ -2,7 +2,10 @@
 
 namespace ActiveCollab\DatabaseStructure\Association;
 
+use ActiveCollab\DatabaseObject\Pool;
 use ActiveCollab\DatabaseStructure\AssociationInterface;
+use ActiveCollab\DatabaseStructure\TypeInterface;
+use Doctrine\Common\Inflector\Inflector;
 use InvalidArgumentException;
 
 /**
@@ -29,5 +32,45 @@ class HasManyViaAssociation extends HasManyAssociation implements AssociationInt
         }
 
         $this->intermediary_type_name = $intermediary_type_name;
+    }
+
+    /**
+     * @param string        $namespace
+     * @param TypeInterface $source_type
+     * @param TypeInterface $target_type
+     * @param array         $result
+     */
+    protected function buildGetFinderMethod($namespace, TypeInterface $source_type, TypeInterface $target_type, array &$result)
+    {
+        $result[] = '';
+        $result[] = '    /**';
+        $result[] = '     * Return ' . Inflector::singularize($source_type->getName()) . ' ' . $this->getName() . ' finder instance';
+        $result[] = '     *';
+        $result[] = '     * @return \\ActiveCollab\\DatabaseObject\\Finder';
+        $result[] = '     */';
+        $result[] = '    private function ' . $this->getFinderMethodName() . '()';
+        $result[] = '    {';
+        $result[] = '       return $this->pool->find(' . var_export($this->getInstanceClassFrom($namespace, $target_type), true) . ')->where("`' . $this->getFkFieldNameFrom($source_type) . '` = ?", $this->getId());';
+        $result[] = '    }';
+    }
+
+    /**
+     * @param string        $namespace
+     * @param TypeInterface $source_type
+     * @param TypeInterface $target_type
+     * @param array         $result
+     */
+    protected function buildCountInstancesMethod($namespace, TypeInterface $source_type, TypeInterface $target_type, array &$result)
+    {
+        $result[] = '';
+        $result[] = '    /**';
+        $result[] = '     * Return number of ' . Inflector::singularize($source_type->getName()) . ' ' . $this->getName();
+        $result[] = '     *';
+        $result[] = '     * @return integer';
+        $result[] = '     */';
+        $result[] = "    public function count{$this->getClassifiedAssociationName()}()";
+        $result[] = '    {';
+        $result[] = '       return $this->pool->count(' . var_export($this->getInstanceClassFrom($namespace, $target_type), true) . ', ["' . $this->getFkFieldNameFrom($source_type) . ' = ?", $this->getId()]);';
+        $result[] = '    }';
     }
 }
