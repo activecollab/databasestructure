9f891efc85aaac865e7fb99ae603be7e32151a95
Ilija Studen <ilija.studen@activecollab.com>
2017-04-17 22:39:39 +0200

    Test required has many association, work in progress

---
+++ test/src/Fixtures/Association/WriterHasManyBooksRequired/WriterHasManyBooksRequiredStructure.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Active Collab DatabaseStructure project.
+ *
+ * (c) A51 doo <info@activecollab.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace ActiveCollab\DatabaseStructure\Test\Fixtures\Association\WriterHasManyBooksRequired;
+
+use ActiveCollab\DatabaseStructure\Association\BelongsToAssociation;
+use ActiveCollab\DatabaseStructure\Association\HasManyAssociation;
+use ActiveCollab\DatabaseStructure\Field\Composite\NameField;
+use ActiveCollab\DatabaseStructure\Structure;
+
+class WriterHasManyBooksRequiredStructure extends Structure
+{
+    public function configure()
+    {
+        $this->addType('writers')->addFields([
+            (new NameField('name', ''))->required(),
+        ])->addAssociations([
+            new HasManyAssociation('books')
+        ]);
+
+        $this->addType('books')->addFields([
+            (new NameField('name', ''))->required(),
+        ])->addAssociations([
+            new BelongsToAssociation('writer')
+        ]);
+    }
+}
