cbc05c98c804c3ea6f12211ca9c7e41974d01f4c
Ilija Studen <ilija.studen@activecollab.com>
2019-12-27 16:40:54 +0100

    Build base directory for base type elements

--- src/Builder/Directories/TypeDirBuilder.php
+++ src/Builder/Directories/TypeDirBuilder.php
@@ -18,6 +18,7 @@ class TypeDirBuilder extends DirBuilder
     {
         return [
             $build_path . '/' . $type->getClassName(),
+            $build_path . '/' . $type->getClassName() . '/Base',
         ];
     }
 }
