93b2198a6c04a4b935f9d671b2db977ca243d4e5
Ilija Studen <ilija.studen@gmail.com>
2022-04-24 23:51:51 +0200

    Remove duplicate geometry read code from polygon

--- src/Field/Scalar/Spatial/PolygonField.php
+++ src/Field/Scalar/Spatial/PolygonField.php
@@ -24,14 +24,4 @@ class PolygonField extends SpatialField
     {
         return 'POLYGON';
     }
-
-    public function getSqlReadStatement(string $table_name): string
-    {
-        return sprintf(
-            "ST_ASTEXT(`%s`.`%s`) AS '%s'",
-            $table_name,
-            $this->getName(),
-            $this->getName()
-        );
-    }
 }
