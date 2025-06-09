485d80ce39110f51afc187159f4bdd2d45929dd0
Ilija Studen <ilija.studen@gmail.com>
2025-06-09 06:59:43 +0200

    Check for file path prior to saving

--- src/Builder/StructureSql.php
+++ src/Builder/StructureSql.php
@@ -32,32 +32,27 @@ trait StructureSql
         $this->appendToSqlFile($this->getInitialDataSqlPath(), $statement, $comment);
     }
 
-    /**
-     * Append statement to a SQL file.
-     *
-     * @param string $file_path
-     * @param string $statement
-     * @param string $comment
-     */
     private function appendToSqlFile(
-        string $file_path,
+        ?string $file_path,
         string $statement,
         string $comment,
     ): void
     {
-        if ($file_path) {
-            $current_content = file_get_contents($file_path);
-
-            if ($current_content) {
-                $current_content .= "\n\n\n\n";
-            }
-
-            if ($comment) {
-                $current_content .= "# $comment\n";
-            }
-
-            file_put_contents($file_path, $current_content . $statement);
+        if (!$file_path) {
+            return;
         }
+
+        $current_content = file_get_contents($file_path);
+
+        if ($current_content) {
+            $current_content .= "\n\n\n\n";
+        }
+
+        if ($comment) {
+            $current_content .= "# $comment\n";
+        }
+
+        file_put_contents($file_path, $current_content . $statement);
     }
     
     abstract public function getBuildPath(): ?string;
